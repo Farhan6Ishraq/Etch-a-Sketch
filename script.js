a472d8e4399f8397b3d670901a3f4a73c2a01406
Farhan6Ishraq <farhanishraqprogramming600@gmail.com>
2022-12-24 17:23:07 +0600

    Added a color choosing option, Added a grid size choosing option

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 const body = document.querySelector("body");
 const grid = document.querySelector(".grid");
+const button1 = document.querySelector('.btn1');
+const colorDiv  = document.querySelector('.color');
+const colorDivChildren = document.querySelectorAll('.color');
 
-for (let i = 1; i <= 16 * 16; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("class", "gridChild");
+function gridSize() {
+  let pixels = Number(prompt('Pick a Grid size(Min: 2, Max: 64)'));
 
-  grid.appendChild(div);
+  if(pixels >= 2 && pixels <= 64) {
+    addDiv(pixels);
+  } else {
+    gridSize();
+  }
 }
 
-const gridChild = document.querySelectorAll(".gridChild");
-
-for (let i = 0; i < gridChild.length; i++) {
-  gridChild[i].addEventListener("mousemove", (e) => {
-    gridChild[i].style.backgroundColor = 'blue';
+function createNew(color) {
+  button1.addEventListener('click', () => {
+    location.reload();
   });
 }
+
+function addDiv(pixels) {
+  for (let i = 1; i <= pixels * pixels; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "gridChild");
+
+    grid.appendChild(div);
+
+  }
+
+  grid.style.gridTemplateColumns = `repeat(${pixels}, auto)`;
+}
+
+function addListener(color) {
+  const gridChild = document.querySelectorAll(".gridChild");
+
+  for (let i = 0; i < gridChild.length; i++) {
+    gridChild[i].addEventListener("mouseover", (e) => {
+      gridChild[i].style.backgroundColor = `${color}`;
+      // button1.style.backgroundColor = `${color}`
+    });
+  }
+}
+
+function bundler() {
+  let color = prompt('Pick a color');
+  createNew(color);
+  gridSize();
+  addListener(color);
+}
+
+bundler();
